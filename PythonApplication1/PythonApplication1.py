ad70e8bbabfbf20eb0e2e6e53d6bda2e6f923ebf
Dmitry Fedorov <astrozzomb@gmail.com>
2023-01-19 00:16:49 +0300

    First commit

---
+++ PythonApplication1/PythonApplication1.py
@@ -0,0 +1,32 @@
+
+#
+def queue_time(customers, n):
+   l = [0]*n
+   
+   for i in range(len(customers)):
+
+      min_value =  min(l)
+
+      indexofmin = l.index(min_value)
+
+      l[indexofmin] += customers[i];  
+
+   return max(l)
+
+
+
+
+
+def solution(s):
+  
+    result = [];
+    for i in range(0,len(s),2):
+        
+        result.append(s[i:i+2])
+
+    if len(s)%2 != 0:
+       result[len(result)-1] +="_"
+
+    return result
+
+ 
